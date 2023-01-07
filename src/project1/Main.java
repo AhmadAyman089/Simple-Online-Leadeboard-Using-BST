fde129f25b624acb4bb114ddbbdf80b329138bb2
Ahmad Ayman <92662407+AhmadAyman089@users.noreply.github.com>
2023-01-07 12:01:26 +0800

    Update Main.java

--- src/project1/Main.java
+++ src/project1/Main.java
@@ -2,112 +2,153 @@ package project1;
 
 import java.util.ArrayList;
 import java.util.List;
-
-// Main class that uses the Leaderboard class
+//Main class that uses the Leaderboard class
 public class Main {
   public static void main(String[] args) {
-    // Create a new Leaderboard instance
     Leaderboard leaderboard = new Leaderboard();
+ // Add some scores to the leaderboard
+    leaderboard.insert("khairul", 75);
+    leaderboard.insert("danial", 50);
+    leaderboard.insert("izz", 25);
+    leaderboard.insert("haikal", 45);
+    leaderboard.insert("aisy", 60);
+    leaderboard.insert("ayman", 80);
+    // Get the top 3 players
+    List<LeaderboardEntry> topPlayers = leaderboard.getTopPlayers(3);
 
-    // Add some scores to the leaderboard
-    leaderboard.addScore("khairul", 75);
-    leaderboard.addScore("danial", 50);
-    leaderboard.addScore("izz", 25);
-    leaderboard.addScore("haikal", 45);
-    leaderboard.addScore("aisy", 60);
-    leaderboard.addScore("ayman", 80);
-    
-
-    // Get the top 2 players
-    List<String> topPlayers = leaderboard.getTopPlayers(2);
-
-    // Print the names of the top players
-    for (String playerName : topPlayers) {
-      System.out.println(playerName);
+    for (LeaderboardEntry player : topPlayers) {
+      System.out.println(player.playerName + ": " + player.score);
     }
   }
 }
+// A class to represent a leaderboard entry (player name and score)
 
-// Class that represents a leaderboard entry with a player's name and score
 class LeaderboardEntry {
-  String playerName;
-  int score;
+	  String playerName;
+	  int score;
 
-  public LeaderboardEntry(String playerName, int score) {
-    this.playerName = playerName;
-    this.score = score;
-  }
-}
+	  public LeaderboardEntry(String playerName, int score) {
+	    this.playerName = playerName;
+	    this.score = score;
+	  }
+	}
 
-// Class that represents a leaderboard
 class Leaderboard {
-  // Inner class that represents a node in the binary search tree
-  private class Node {
-    LeaderboardEntry entry;
-    Node left;
-    Node right;
+	  // A class to represent a node in the binary search tree
+	  private class Node {
+	    LeaderboardEntry entry;
+	    Node left;
+	    Node right;
 
-    public Node(LeaderboardEntry entry) {
-      this.entry = entry;
-    }
-  }
+	    public Node(LeaderboardEntry entry) {
+	      this.entry = entry;
+	    }
+	  }
 
-  Node root;
+	  Node root;
 
-  // Method that adds a new score to the leaderboard
-  public void addScore(String playerName, int score) {
-    // Create a new LeaderboardEntry object
-    LeaderboardEntry entry = new LeaderboardEntry(playerName, score);
-    // Insert the entry into the binary search tree
-    root = addScore(root, entry);
-  }
+	  // Inserts a new player and their score into the tree
+	  public void insert(String playerName, int score) {
+	    LeaderboardEntry entry = new LeaderboardEntry(playerName, score);
+	    root = insert(root, entry);
+	  }
 
-  // Recursive helper method for adding a score to the leaderboard
-  private Node addScore(Node node, LeaderboardEntry entry) {
-    if (node == null) {
-      // Return a new node if the tree is empty
-      return new Node(entry);
-    }
+	  private Node insert(Node node, LeaderboardEntry entry) {
+	    if (node == null) {
+	      return new Node(entry);
+	    }
 
-    if (entry.score > node.entry.score) {
-      // Add the entry to the right subtree if its score is greater than the current node's score
-      node.right = addScore(node.right, entry);
-    } else {
-      // Add the entry to the left subtree if its score is less than or equal to the current node's score
-      node.left = addScore(node.left, entry);
-    }
+	    if (entry.score < node.entry.score) {
+	      node.left = insert(node.left, entry);
+	    } else if (entry.score > node.entry.score) {
+	      node.right = insert(node.right, entry);
+	    } else {
+	      // Update the score if the player already exists in the tree
+	      node.entry.score = entry.score;
+	    }
 
-    return node;
-  }
+	    return node;
+	  }
 
-  // Method that returns the top n players sorted in descending order of their scores
-  public List<String> getTopPlayers(int n) {
-    // Create a list to store the top players
-    List<String> topPlayers = new ArrayList<>();
-    // Traverse the tree in the reverse in-order traversal order and add the player names to the list
-    getTopPlayers(root, n, topPlayers);
-    return topPlayers;
-  }
+	  // Searches for a player in the tree and returns their score, or -1 if not found
+	  public int search(String playerName) {
+	    Node node = search(root, playerName);
+	    return node == null ? -1 : node.entry.score;
+	  }
 
-  // Recursive helper method for getting the top players
-  private void getTopPlayers(Node node, int n, List<String> topPlayers) {
-    if (node == null || topPlayers.size() == n) {
-      // Return if the tree is empty or the list has reached the required size
-      return;
-    }
+	  private Node search(Node node, String playerName) {
+	    if (node == null) {
+	      return null;
+	    }
 
- // Traverse the right subtree
-    getTopPlayers(node.right, n, topPlayers);
+	    if (playerName.equals(node.entry.playerName)) {
+	      return node;
+	    } else if (playerName.compareTo(node.entry.playerName) < 0) {
+	      return search(node.left, playerName);
+	    } else {
+	      return search(node.right, playerName);
+	    }
+	  }
 
-    if (topPlayers.size() < n) {
-      // Add the player name to the list if the list is not full yet
-      topPlayers.add(node.entry.playerName);
-    }
+	  // Deletes a player from the tree
+	  public void delete(String playerName) {
+	    root = delete(root, playerName);
+	  }
 
-    // Traverse the left subtree
-    getTopPlayers(node.left, n, topPlayers);
-    
-  }
-}
+	  private Node delete(Node node, String playerName) {
+	    if (node == null) {
+	      return null;
+	    }
 
+	    if (playerName.equals(node.entry.playerName)) {
+	      if (node.left == null && node.right == null) {
+	        return null;
+	      } else if (node.left == null) {
+	        return node.right;
+	      } else if (node.right == null) {
+	        return node.left;
+	      } else {
+	        // Find the minimum value in the right subtree
+	        Node minNode = findMin(node.right);
+	        node.entry = minNode.entry;
+	        node.right = delete(node.right, minNode.entry.playerName);
+	        return node;
+	      }
+	    } else if (playerName.compareTo(node.entry.playerName) < 0) {
+	      node.left = delete(node.left, playerName);
+	      return node;
+	    } else {
+	      node.right = delete(node.right, playerName);
+	      return node;
+	    }
+	  }
+
+	  // Finds the node with the minimum value in a given
+	  private Node findMin(Node node) {
+		    while (node.left != null) {
+		      node = node.left;
+		    }
+		    return node;
+		  }
+
+		  public List<LeaderboardEntry> getTopPlayers(int n) {
+		    List<LeaderboardEntry> topPlayers = new ArrayList<>();
+		    getTopPlayers(root, n, topPlayers);
+		    return topPlayers;
+		  }
+
+		  private void getTopPlayers(Node node, int n, List<LeaderboardEntry> topPlayers) {
+		    if (node == null || topPlayers.size() == n) {
+		      return;
+		    }
+
+		    getTopPlayers(node.right, n, topPlayers);
+
+		    if (topPlayers.size() < n) {
+		      topPlayers.add(node.entry);
+		    }
+
+		    getTopPlayers(node.left, n, topPlayers);
+		  }
+		}
 
